73f7f66421e4b40c715044dae4896db7b66c1510
PauloHDSousa <paulo.ti.sousa@gmail.com>
2018-11-06 22:17:17 -0200

    #First Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+    public float increment;
+    public float maxY;
+    public float minY;
+
+    private Vector2 targetPos;
+
+    public int health;
+
+    public GameObject moveEffect;
+    public Animator camAnim;
+    public Text healthDisplay;
+    public GameObject scoreManager;
+    public GameObject spawner;
+    public GameObject restartDisplay;
+    AudioSource audioSource;
+    Animator animator;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+
+        if (health <= 0) {
+            spawner.SetActive(false);
+            scoreManager.SetActive(false);
+            restartDisplay.SetActive(true);
+            Destroy(gameObject);
+        }
+
+        healthDisplay.text = health.ToString();
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxY) {
+            camAnim.SetTrigger("shake");
+            Instantiate(moveEffect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y + increment);
+            audioSource.PlayOneShot(audioSource.clip);
+            animator.SetTrigger("Teleport");
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minY) {
+            camAnim.SetTrigger("shake");
+            Instantiate(moveEffect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y - increment);
+            audioSource.PlayOneShot(audioSource.clip);
+            animator.SetTrigger("Teleport");
+        }
+    }
+}
---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+}
